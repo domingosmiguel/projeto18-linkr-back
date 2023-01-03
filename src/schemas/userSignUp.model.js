a4eb318479685d29ba845b0db0df8136c516e96a
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2023-01-03 14:35:29 -0300

    fix: username max and min size

--- src/schemas/userSignUp.model.js
+++ src/schemas/userSignUp.model.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const userModel = Joi.object({
-  username: Joi.string().min(4).max(20).required(),
+  username: Joi.string().min(3).max(40).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).max(50).required(),
   pictureUrl: Joi.string()
