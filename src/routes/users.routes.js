781e96efc40b2cc3308f6c2e67f7d736e4571b07
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2023-01-03 16:45:52 -0300

    fix: add schema validation function to sign up

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { signIn, userSignUp } from '../controllers/users.controller.js';
 import signInMiddleware from '../middlewares/signIn.middleware.js';
+import validateUserSignUpSchema from '../middlewares/validateUserSignUpModel.middleware.js';
 
 const router = Router();
 
-router.post('/signup', userSignUp);
+router.post('/signup', validateUserSignUpSchema, userSignUp);
 
 router.post('/signin', signInMiddleware, signIn);
 
