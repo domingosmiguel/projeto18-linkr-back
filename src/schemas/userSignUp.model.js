b5dafce4cb5917746c06a27001615ae68403adc2
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2023-01-03 13:34:40 -0300

    fix: user sign up schema

--- src/schemas/userSignUp.model.js
+++ src/schemas/userSignUp.model.js
@@ -1,8 +1,10 @@
 import Joi from 'joi';
 
 export const userModel = Joi.object({
-  username: Joi.string().required(),
+  username: Joi.string().min(4).max(20).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
-  pictureUrl: Joi.string().required(),
+  password: Joi.string().min(6).max(50).required(),
+  pictureUrl: Joi.string()
+    .pattern(new RegExp('https?://(www.)?[^/]*?/?([^$]*?$)?'))
+    .required(),
 });
