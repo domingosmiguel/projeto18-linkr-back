a081686c6499066b3e3e149b76b7f341d1f0fa5d
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2023-01-06 16:43:40 -0300

    fix: query w/ correct data

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,21 +4,24 @@ import connection from '../database.js';
 
 dotenv.config();
 
-export default async function authMiddleware(req, res, next) {
-  dotenv.config();
+export default async function jwtValidation(req, res, next) {
   const { authorization } = req.headers;
   const token = authorization?.replace('Bearer ', '');
 
   try {
-    const { sessionId } = jwt.verify(token, process.env.JWT_SECRET);
+    const {
+      sessionId: {
+        rows: [{ id }],
+      },
+    } = jwt.verify(token, process.env.JWT_SECRET);
     const { rowCount: loginAuthorized } = await connection.query(
       'SELECT * FROM sessions WHERE id = $1',
-      [sessionId]
+      [id]
     );
     if (!loginAuthorized) throw new Error();
     res.locals.userId = loginAuthorized.userId;
     next();
   } catch {
-    res.status(401).send('Access denied');
+    res.status(401).send({ message: 'Access denied' });
   }
 }
