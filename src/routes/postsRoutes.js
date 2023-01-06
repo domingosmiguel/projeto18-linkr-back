9bc2b5886f3514d15a51ce6f3f5e2fd205197e5a
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2023-01-05 22:58:20 -0300

    chore: get timeline route responds all info

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,12 +1,17 @@
-import { Router } from "express";
+import { Router } from 'express';
 // import jwtValidation from "../middlewares/auth.middleware.js"
-import { postTimelinePosts, getTimelinePosts } from "../controllers/postControllers.js";
-import { postMiddleware } from "../middlewares/postMiddleware.js";
+import {
+  postTimelinePosts,
+  getTimelinePosts,
+} from '../controllers/postControllers.js';
+import jwtValidation from '../middlewares/auth.middleware.js';
+import getUserInfo from '../middlewares/getUserInfo.middleware.js';
+import { postMiddleware } from '../middlewares/postMiddleware.js';
 
 const router = Router();
 
-router.post("/timeline-posts", postMiddleware, postTimelinePosts);
+router.post('/timeline-posts', postMiddleware, postTimelinePosts);
 
-router.get("/timeline-posts", getTimelinePosts);
+router.get('/timeline-posts', jwtValidation, getUserInfo, getTimelinePosts);
 
 export default router;
